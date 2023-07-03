7992a52c5c059d03401b64e01f0ead8e09f9b4ba
paul.wu <tease0623@gmail.com>
2023-07-03 17:05:24 +0800

    expense-tracker project init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', exphbs.engine(
+    {
+        defaultLayout: 'main',
+        extname: 'hbs'
+    }
+))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
---
+++ views/index.hbs
@@ -0,0 +1,1 @@
+<h1>expense-tracker</h1>
